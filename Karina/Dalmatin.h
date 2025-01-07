4c0febffabcf1233a762de858b8b0c5d1789aa4d
Karina <14150226c@gmail.com>
2025-01-07 17:42:37 +0700

    Create Dalmatin.h

---
+++ Karina/Dalmatin.h
@@ -0,0 +1,20 @@
+#pragma warning(disable : 4996)
+#define CRT_SECURE_NO_WARNINGS 
+#include <iostream>
+#include "string.h"
+#include <locale.h>
+#include <conio.h>
+#include <stdio.h>
+
+class Dalmatin {
+private:
+    bool found;
+
+public:
+    Dalmatin() {}
+    Dalmatin(bool found) {
+        this->found = found;
+    }
+
+    bool getDalmatinFound() { return found; }
+};
